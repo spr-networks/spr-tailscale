e2e891ec4a5029832698f50103811a850ddb7eda
Alex Radocea <alex@supernetworks.org>
2023-11-25 13:17:21 -0800

    [spr-tailscale] move around firewall changes

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -8,4 +8,12 @@ tailscaled & >$TAILSCALE_STATE_DIR/stdout.log 2>$TAILSCALE_STATE_DIR/stderr.log
 
 . /scripts/up.sh
 
+
+###TBD fix this race, see rebuildPostrouting
+sleep 3
+
+# forward from SPR into the tailscale network
+nft add chain ip filter POSTROUTING { type nat hook postrouting priority 100 \; }
+nft add rule ip filter POSTROUTING oif "tailscale0" masquerade
+
 /tailscale_plugin
