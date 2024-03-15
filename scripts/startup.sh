8a72196d454ed1f516e23381c71c213f31dcc7ed
Alex Radocea <alex@supernetworks.org>
2024-03-15 09:23:21 -0700

    [spr-tailscale] Rely on daemon for init

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,18 +2,14 @@
 set -a
 . /configs/spr-tailscale/config.sh
 
-TAILSCALE_STATE_DIR=/state/spr-tailscale/tailscaled
-
-tailscaled & >$TAILSCALE_STATE_DIR/stdout.log 2>$TAILSCALE_STATE_DIR/stderr.log
-
-. /scripts/up.sh
-
-
+#TAILSCALE_STATE_DIR=/state/spr-tailscale/tailscaled
+#tailscaled & >$TAILSCALE_STATE_DIR/stdout.log 2>$TAILSCALE_STATE_DIR/stderr.log
+#. /scripts/up.sh
 ###TBD fix this race, see rebuildPostrouting
-sleep 3
+#sleep 3
 
 # forward from SPR into the tailscale network
-nft add chain ip filter POSTROUTING { type nat hook postrouting priority 100 \; }
-nft add rule ip filter POSTROUTING oif "tailscale0" masquerade
+#nft add chain ip filter POSTROUTING { type nat hook postrouting priority 100 \; }
+#nft add rule ip filter POSTROUTING oif "tailscale0" masquerade
 
 /tailscale_plugin
