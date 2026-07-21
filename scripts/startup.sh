6c1260c8233147294e6bee5bf6e8329533f51d4a
Alex Rad <alex@supernetworks.org>
2026-07-21 10:55:44 -0700

    Enable forwarding inside the KVM guest

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,11 @@ set -a
 . /configs/base/config.sh
 . /configs/spr-tailscale/config.sh
 
+sysctl -q -w net.ipv4.ip_forward=1
+if [ -e /proc/sys/net/ipv6/conf/all/forwarding ]; then
+    sysctl -q -w net.ipv6.conf.all.forwarding=1
+fi
+
 # TS_STATE_DIR is set in docker-compose.yml
 tailscaled & >$TAILSCALE_STATE_DIR/stdout.log 2>$TAILSCALE_STATE_DIR/stderr.log
 
